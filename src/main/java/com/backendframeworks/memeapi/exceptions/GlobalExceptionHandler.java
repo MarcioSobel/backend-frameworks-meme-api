dc5755a71453a66241314d45e7af0223b7a302cf
MarcioSobel <marcinhosobel@hotmail.com>
2024-11-24 18:45:15 -0300

    :recycle: refactor: refactor global exception handler

--- src/main/java/com/backendframeworks/memeapi/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/backendframeworks/memeapi/exceptions/GlobalExceptionHandler.java
@@ -9,14 +9,22 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(Conflict.class)
-	public ResponseEntity<HttpError> handleConflictException(Conflict exception) {
-		HttpError httpError = new HttpError(HttpStatus.CONFLICT, exception.getMessage());
-		return ResponseEntity.status(httpError.getStatus()).body(httpError);
+	public ResponseEntity<HttpError> handleConflictException(Conflict e) {
+		return generateResponseEntity(HttpStatus.CONFLICT, e);
 	}
 
 	@ExceptionHandler(Unauthorized.class)
-	public ResponseEntity<HttpError> handleUnauthorizedException(Unauthorized exception) {
-		HttpError httpError = new HttpError(HttpStatus.UNAUTHORIZED, exception.getMessage());
-		return ResponseEntity.status(httpError.getStatus()).body(httpError);
+	public ResponseEntity<HttpError> handleUnauthorizedException(Unauthorized e) {
+		return generateResponseEntity(HttpStatus.UNAUTHORIZED, e);
+	}
+
+	@ExceptionHandler(NotFound.class)
+	public ResponseEntity<HttpError> handleNotFoundException(NotFound e) {
+		return generateResponseEntity(HttpStatus.NOT_FOUND, e);
+	}
+
+	private ResponseEntity<HttpError> generateResponseEntity(HttpStatus status, RuntimeException exception) {
+		HttpError httpError = new HttpError(status, exception.getMessage());
+		return ResponseEntity.status(status).body(httpError);
 	}
 }
